49031ee10d1311391c100ec0763bdd417479b061
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-02-16 03:35:59 -0800

    Exercise 1 and Exercise 2 was completed with several editing and some codes might look a little different.

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace OperatorExercise
+﻿using System.Formats.Asn1;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //------Exercise 1-------------
+
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("Please eneter the radius of your circle?");
+            var userInput = Console.ReadLine();
+            var radius = double.Parse(userInput);
+            Console.WriteLine(AreaOfCircle(radius));
         }
+        //-----Exercise 2---------
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return Math.Sqrt(area);
+        }
+
     }
+
 }
+
+        
+      
+
+    
+
+         
+            
+    
+
